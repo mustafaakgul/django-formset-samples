3b352636386bace5ee5e98f8a8e8c1f4418e575b
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-03-25 23:35:11 +0300

    Upd base

--- formset5/test10/urls.py
+++ formset5/test10/urls.py
@@ -1,4 +1,4 @@
-"""test10 URL Configuration
+"""formset5 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- formset5/test10/wsgi.py
+++ formset5/test10/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for test10 project.
+WSGI config for formset5 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test10.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'formset5.settings')
 
 application = get_wsgi_application()
