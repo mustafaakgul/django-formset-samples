e06ca243bea95fa594956e1f71f05318f162b94f
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-03-25 23:35:26 +0300

    Upd base

--- formset6/test11/urls.py
+++ formset6/test11/urls.py
@@ -1,4 +1,4 @@
-"""test11 URL Configuration
+"""formset6 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
