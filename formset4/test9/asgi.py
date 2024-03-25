fd0b50e8ae3f3e5e4662e7463309d7489c87606e
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-03-25 23:34:53 +0300

    Upd base

--- formset4/test9/asgi.py
+++ formset4/test9/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for test9 project.
+ASGI config for formset4 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test9.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'formset4.settings')
 
 application = get_asgi_application()
